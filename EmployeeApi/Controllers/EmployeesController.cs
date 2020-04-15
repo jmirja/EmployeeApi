d82df9296e3c44645aa30f5d9a2ed414bfb757c7
jmirja <41453618+jmirja@users.noreply.github.com>
2020-04-16 02:37:34 +0600

    Add project files.

---
+++ EmployeeApi/Controllers/EmployeesController.cs
@@ -0,0 +1,22 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeApi.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get() 
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities()) 
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
