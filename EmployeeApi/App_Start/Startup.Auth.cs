3ee9351443c586c0a5a9343af4dd90e3e5173f21
jmirja <41453618+jmirja@users.noreply.github.com>
2020-04-23 03:05:09 +0600

    using sessionStorage Login and Data page created.

--- EmployeeApi/App_Start/Startup.Auth.cs
+++ EmployeeApi/App_Start/Startup.Auth.cs
@@ -38,7 +38,7 @@ namespace EmployeeApi
                 TokenEndpointPath = new PathString("/Token"),
                 Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
-                AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
+                AccessTokenExpireTimeSpan = TimeSpan.FromSeconds(5),
                 // In production mode set AllowInsecureHttp = false
                 AllowInsecureHttp = true
             };
